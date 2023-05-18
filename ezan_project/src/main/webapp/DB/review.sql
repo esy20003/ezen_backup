daf78fa36da4ce7bc28b3f892722ce54baebcccf
JAVA01 <JAVA01@DESKTOP-LRKDKME>
2023-05-18 15:01:09 +0900

    Commit

--- ezan_project/src/main/webapp/DB/review.sql
+++ ezan_project/src/main/webapp/DB/review.sql
@@ -1,32 +1,32 @@
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '아이유 콘서트 성공후기', '1열로 성공해주셨어요! 감사합니다 믿고 맡기세요')
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 1, '아이유 콘서트 성공후기', '1열로 성공해주셨어요! 감사합니다 믿고 맡기세요','dsan');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '콘서트 성공 후기!!', '솔직히 금액이 커서 고민 많이했는데 좋은 자리 잡아주셨어용 ㅜㅜ 너무 감사해요'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 2, '콘서트 성공 후기!!', '솔직히 금액이 커서 고민 많이했는데 좋은 자리 잡아주셨어용 ㅜㅜ 너무 감사해요','bts2');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '아이브 팬미팅 성공', '이번에 꼭 가고 싶었는데 좋은 자리로 잡아주셨어요 너무 감사합니다 다음번에 또 맡기러 오겠습니다'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 3, '아이브 팬미팅 성공', '이번에 꼭 가고 싶었는데 좋은 자리로 잡아주셨어요 너무 감사합니다 다음번에 또 맡기러 오겠습니다','gangji');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '뮤지컬 데스노트', '막공 꼭 가고 싶었는데 잡아주셨어요 원하는 자리라 너무 좋아요 복 받으세요!'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 4, '뮤지컬 데스노트', '막공 꼭 가고 싶었는데 잡아주셨어요 원하는 자리라 너무 좋아요 복 받으세요!','jrong');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '샤이니 팬미팅 성공 후기', '긴가민가했는데 다행히도 원하는 자리네요 확실히 믿고 맡기셔도 좋을 것 같아요'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 5, '샤이니 팬미팅 성공 후기', '긴가민가했는데 다행히도 원하는 자리네요 확실히 믿고 맡기셔도 좋을 것 같아요','kinder');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '꺅 성공후기!!', '제가 너무너무 보고 싶었던 연극이었는데 완전 꿀자리 잡아주셨엉숑 ㅠㅠ!! 너무너무 감사하고 다음에 또 올게용!! 부디 건강하세용!!'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 6, '꺅 성공후기!!', '제가 너무너무 보고 싶었던 연극이었는데 완전 꿀자리 잡아주셨엉숑 ㅠㅠ!! 너무너무 감사하고 다음에 또 올게용!! 부디 건강하세용!!','babo');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '선생님 감사합니다', '선생님 진짜 이 은혜를 어찌 갚아야할지... 너무너무 행복합니다 ㅠㅠ 똥손도 좋은 자리를 가보는군요 허허'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 7, '선생님 감사합니다', '선생님 진짜 이 은혜를 어찌 갚아야할지... 너무너무 행복합니다 ㅠㅠ 똥손도 좋은 자리를 가보는군요 허허','gildong');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, 'lol 성공후기', '빡센 티켓팅이라 걱정 많이했는데 딱 잡아주셨네요 밑고 맡기셔도 될 것 같습니다!'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 8, 'lol 성공후기', '빡센 티켓팅이라 걱정 많이했는데 딱 잡아주셨네요 밑고 맡기셔도 될 것 같습니다!','work');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '힙합페스티벌 1열 후기', '와 진짜 1열로 성공해주시다니 정말 감사합니다 여러분도 다들 이분께 맡기세요 굿굿'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 9, '힙합페스티벌 1열 후기', '와 진짜 1열로 성공해주시다니 정말 감사합니다 여러분도 다들 이분께 맡기세요 굿굿','sleep');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '뮤지컬 성공후기', '뮤덕이라 항상 가는데 여기만큼 좋은 자리 잡아주는 곳 없었어요 항상 맡기러 오겠습니다'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 10, '뮤지컬 성공후기', '뮤덕이라 항상 가는데 여기만큼 좋은 자리 잡아주는 곳 없었어요 항상 맡기러 오겠습니다','java');
 
-insert into review_board(rseq, title, content)
-values(review_board_seq.nextval, '연극 환상동화 후기', '진짜 너무너무 기뻐요 ㅠㅠ 제가 이런 자리 가도 되는 걸까요 ㅠㅠ 진짜 너무너무 행복합니다 다음에 또 오겠습니다 ㅠㅠ'
+insert into review_board(rseq, mseq, title, content, id)
+values(review_board_seq.nextval, 11, '연극 환상동화 후기', '진짜 너무너무 기뻐요 ㅠㅠ 제가 이런 자리 가도 되는 걸까요 ㅠㅠ 진짜 너무너무 행복합니다 다음에 또 오겠습니다 ㅠㅠ','nomoney');
