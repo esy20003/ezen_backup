f91012417d0bf5632379a1c99aaba2b3d9f6bee8
KuMMii <4.prgmng@gmail.com>
2023-05-19 00:00:41 +0900

    add: apply/register.jsp  & action
    apply_Or_Register.jsp
    ApplyAndRegisterAction.java 생성

--- ezen_pj/src/main/java/com/ezen/ticket/controller/action/anr/ApplyAndRegisterAction.java
+++ ezen_pj/src/main/java/com/ezen/ticket/controller/action/anr/ApplyAndRegisterAction.java
@@ -2,16 +2,20 @@ package com.ezen.ticket.controller.action.anr;
 
 import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import com.ezen.ticket.controller.action.Action;
-import com.ezen.ticket.controller.action.HttpServletRequest;
-import com.ezen.ticket.controller.action.HttpServletResponse;
-import com.ezen.ticket.controller.action.ServletException;
 
 public class ApplyAndRegisterAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
+		String url="apply_register/apply_Or_Register.jsp";
+		
+		request.getRequestDispatcher(url).forward(request, response);
 	}
 
 }
