846e3c9b3b6c76b8e3427b40626074fa6921fa0f
JAVA01 <JAVA01@DESKTOP-LRKDKME>
2023-05-18 14:38:58 +0900

    Qna

--- ezan_project/src/main/webapp/DB/qna.sql
+++ ezan_project/src/main/webapp/DB/qna.sql
@@ -1,36 +1,36 @@
 insert into qna_board(qseq, mseq, title, content, id)
-values(SEQ_QNA_BOARD_QSEQ, 1, '종료된 티켓팅', '종료된 티켓팅도 취켓팅으로 가능한가요?','dsan');
+values (qna_board_seq.nextval, 1, '종료된 티켓팅', '종료된 티켓팅도 취켓팅으로 가능한가요?','dsan');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '문의합니다', '수고비는 자리에 따라 조정되나요?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 2, '문의합니다', '수고비는 자리에 따라 조정되나요?', 'bts2');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '문의드립니다~', '사이트에 없는 티켓팅도 가능한가요?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 3, '문의드립니다~', '사이트에 없는 티켓팅도 가능한가요?', 'gangji');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '수고비 문의', '취소하면 전액 환불 해주시나요?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 4, '수고비 문의', '취소하면 전액 환불 해주시나요?', 'jrong');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '환불문의', '아직 환불이 안 들어왔는데 언제쯤 들어올까요?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 5, '환불문의', '아직 환불이 안 들어왔는데 언제쯤 들어올까요?', 'kinder');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '문의합니다!', '원하는 열과 구역을 정확히 지정할 수 있을까요?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 6, '문의합니다!', '원하는 열과 구역을 정확히 지정할 수 있을까요?', 'babo');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '취소 문의', '취소하고 싶어요 ㅠㅠ 취소 가능할까요?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 7, '취소 문의', '취소하고 싶어요 ㅠㅠ 취소 가능할까요?', 'gildong');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '문의드려용!!', '꼭 성공 부탁드리겠습니당!!?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 8, '문의드려용!!', '꼭 성공 부탁드리겠습니당!!?', 'work');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '아이디 변경 문의', '아이디와 비번을 변경할 수 있을까요?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 9, '아이디 변경 문의', '아이디와 비번을 변경할 수 있을까요?', 'sleep');
 
-insert into qna_board(qseq, title, content)
-values(qna_board_seq.nextval, '문희드립니당', '수고비도 조정이 가능한가요?')
+insert into qna_board(qseq, mseq, title, content, id)
+values(qna_board_seq.nextval, 10, '문희드립니당', '수고비도 조정이 가능한가요?', 'java');
 
 select * from REVIEW_BOARD
 select * from MEMBER
-
-
+select * from qna_board
+select * from seat
 
 
