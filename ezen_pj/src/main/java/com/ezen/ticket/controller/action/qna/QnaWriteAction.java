e02c08704580c3a4fd3f472b5821f5189db76107
JAVA01 <JAVA01@DESKTOP-LRKDKME>
2023-05-19 10:35:56 +0900

    Merge branch 'main' of https://github.com/chojeajun/ezen_project.git into main

--- ezen_pj/src/main/java/com/ezen/ticket/controller/action/qna/QnaWriteAction.java
+++ ezen_pj/src/main/java/com/ezen/ticket/controller/action/qna/QnaWriteAction.java
@@ -1,6 +1,7 @@
 package com.ezen.ticket.controller.action.qna;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -8,6 +9,8 @@ import javax.servlet.http.HttpSession;
 
 import com.ezen.ticket.controller.action.Action;
 import com.ezen.ticket.dao.QnaDao;
+import com.ezen.ticket.dto.MemberVO;
+import com.ezen.ticket.dto.QnaVO;
 
 public class QnaWriteAction implements Action {
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
