702750af70584558c0266f27af52154b0986a2a7
esy20003 <esy20003@gmail.com>
2023-05-19 11:33:50 +0900

    Feat : ActionFactory loginAction MemberDao 추가

--- ezen_pj/src/main/java/com/ezen/ticket/controller/ActionFactory.java
+++ ezen_pj/src/main/java/com/ezen/ticket/controller/ActionFactory.java
@@ -10,6 +10,7 @@ import com.ezen.ticket.controller.action.qna.QnaWriteFormAction;
 import com.ezen.ticket.controller.action.member.JoinFormAction;
 import com.ezen.ticket.controller.action.member.LoginAction;
 import com.ezen.ticket.controller.action.member.LoginFormAction;
+import com.ezen.ticket.controller.action.member.LogoutAction;
 
 
 public class ActionFactory {
@@ -32,10 +33,12 @@ public class ActionFactory {
 		else if (command.equals("applyAndRegister")) ac = new ApplyAndRegisterAction();
 		
 		
-
+		// login
 		else if(command.equals("loginForm")) ac = new LoginFormAction();
 		else if(command.equals("login")) ac = new LoginAction();
-		
+		else if(command.equals("logout")) ac = new LogoutAction();
+		 
+		// join
 		else if(command.equals("joinForm")) ac = new JoinFormAction();
 	
 		return ac;
