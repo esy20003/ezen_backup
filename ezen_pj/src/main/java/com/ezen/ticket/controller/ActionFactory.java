267f62c2e515f746cd2b49fc935e083a7c3d2f15
KuMMii <4.prgmng@gmail.com>
2023-05-19 11:29:08 +0900

    Feat: applyForm.jsp

--- ezen_pj/src/main/java/com/ezen/ticket/controller/ActionFactory.java
+++ ezen_pj/src/main/java/com/ezen/ticket/controller/ActionFactory.java
@@ -3,17 +3,9 @@ package com.ezen.ticket.controller;
 import com.ezen.ticket.controller.action.Action;
 import com.ezen.ticket.controller.action.IndexAction;
 import com.ezen.ticket.controller.action.anr.ApplyAndRegisterAction;
-
 import com.ezen.ticket.controller.action.anr.RegisterFormAction;
-
-
 import com.ezen.ticket.controller.action.anr.ApplyFormAction;
-<<<<<<< HEAD
-
-
-=======
 import com.ezen.ticket.controller.action.anr.RegisterFormAction;
->>>>>>> branch 'main' of https://github.com/chojeajun/ezen_project.git
 import com.ezen.ticket.controller.action.qna.QnaListAction;
 import com.ezen.ticket.controller.action.qna.QnaViewAction;
 import com.ezen.ticket.controller.action.qna.QnaWriteAction;
@@ -40,9 +32,7 @@ public class ActionFactory {
 		else if (command.equals("qnaWrite")) ac = new QnaWriteAction();
 		
 		else if (command.equals("applyAndRegister")) ac = new ApplyAndRegisterAction();
-
 		else if (command.equals("registerForm")) ac = new RegisterFormAction();
-
 		else if (command.equals("applyForm")) ac = new ApplyFormAction();
 		
 
