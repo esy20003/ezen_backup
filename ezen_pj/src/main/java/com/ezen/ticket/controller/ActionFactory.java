6fc96eb85a95210b726758e6cde8034aec9fc502
KuMMii <4.prgmng@gmail.com>
2023-05-19 10:31:02 +0900

    Feat: added js, new jsp
    go_apply() 추가, applyFormAction 추가

--- ezen_pj/src/main/java/com/ezen/ticket/controller/ActionFactory.java
+++ ezen_pj/src/main/java/com/ezen/ticket/controller/ActionFactory.java
@@ -3,6 +3,7 @@ package com.ezen.ticket.controller;
 import com.ezen.ticket.controller.action.Action;
 import com.ezen.ticket.controller.action.IndexAction;
 import com.ezen.ticket.controller.action.anr.ApplyAndRegisterAction;
+import com.ezen.ticket.controller.action.anr.ApplyFormAction;
 import com.ezen.ticket.controller.action.qna.QnaListAction;
 import com.ezen.ticket.controller.action.qna.QnaViewAction;
 import com.ezen.ticket.controller.action.qna.QnaWriteAction;
@@ -29,6 +30,7 @@ public class ActionFactory {
 		else if (command.equals("qnaWrite")) ac = new QnaWriteAction();
 		
 		else if (command.equals("applyAndRegister")) ac = new ApplyAndRegisterAction();
+		else if (command.equals("applyForm")) ac = new ApplyFormAction();
 		
 		
 
--- ezen_pj/src/main/webapp/apply_register/apply_register.js
+++ ezen_pj/src/main/webapp/apply_register/apply_register.js
@@ -3,5 +3,6 @@
  */
  
  function go_apply(){
-	 
+	 bodyChoose.applyButton.action='ticket.do?command=applyForm';
+	 bodyChoose.applyButton.submit();
  }
