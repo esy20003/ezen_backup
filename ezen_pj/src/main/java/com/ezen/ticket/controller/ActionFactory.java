8b9eb24be78dfb5acf11755b837d968e3a46bb50
chojeajun <131836874+chojeajun@users.noreply.github.com>
2023-05-19 10:41:52 +0900

    feat: js, registerFromAction 추가

--- ezen_pj/src/main/java/com/ezen/ticket/controller/ActionFactory.java
+++ ezen_pj/src/main/java/com/ezen/ticket/controller/ActionFactory.java
@@ -7,7 +7,7 @@ import com.ezen.ticket.controller.action.anr.ApplyAndRegisterAction;
 import com.ezen.ticket.controller.action.anr.RegisterViewAction;
 
 import com.ezen.ticket.controller.action.anr.ApplyFormAction;
-
+import com.ezen.ticket.controller.action.anr.RegisterFormAction;
 import com.ezen.ticket.controller.action.qna.QnaListAction;
 import com.ezen.ticket.controller.action.qna.QnaViewAction;
 import com.ezen.ticket.controller.action.qna.QnaWriteAction;
@@ -35,7 +35,7 @@ public class ActionFactory {
 		
 		else if (command.equals("applyAndRegister")) ac = new ApplyAndRegisterAction();
 
-		else if (command.equals("registerView")) ac = new RegisterViewAction();
+		else if (command.equals("registerForm")) ac = new RegisterFormAction();
 
 		else if (command.equals("applyForm")) ac = new ApplyFormAction();
 		
