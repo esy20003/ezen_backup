ee1b072fd5c71ef51d785f4750405390a3b35752
whatmymoon <103135344+whatmymoon@users.noreply.github.com>
2023-05-23 16:32:48 +0900

    main 화면 백엔드 구성(bestContent, Concert, Musical 완료) (success 하는중이었습니다.)

--- ezen_pj/src/main/java/com/ezen/ticket/controller/action/IndexAction.java
+++ ezen_pj/src/main/java/com/ezen/ticket/controller/action/IndexAction.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.ezen.ticket.dao.ContentDao;
 import com.ezen.ticket.dto.ContentVO;
+import com.ezen.ticket.dto.SuccessVO;
 
 public class IndexAction implements Action {
 
@@ -29,13 +30,17 @@ public class IndexAction implements Action {
 		 * dispatcher.forward(request, response);
 		 */
 		
-		// bestContent 받아오기
+		// best, concert, musical 받아오기
 		ContentDao cdao = ContentDao.getInstance();
 		ArrayList<ContentVO> bestContent = cdao.getBestContent();
-		
 		ArrayList<ContentVO> concert = cdao.getConsert();
+		ArrayList<ContentVO> musical = cdao.getMusical();
+		/* ArrayList<SuccessVO> success = sdao.getSuccess(); */
 		
 		request.setAttribute("bestContent", bestContent);
+		request.setAttribute("concert", concert);
+		request.setAttribute("musical", musical);
+		/* request.setAttribute("success", success); */
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher("main.jsp");
 		dispatcher.forward(request, response);
--- ezen_pj/src/main/java/com/ezen/ticket/dao/ContentDao.java
+++ ezen_pj/src/main/java/com/ezen/ticket/dao/ContentDao.java
@@ -19,30 +19,31 @@ public class ContentDao {
 	ResultSet rs = null;
 	
 	public ArrayList<ContentVO> getBestContent() {
-		ArrayList<ContentVO> list = null;
-		ContentVO cvo = new ContentVO();
-		String sql = "select * from content where bestyn=?";
-		con = Dbman.getConnection();
-		try {
-			pstmt = con.prepareStatement(sql);
-			sql = "select * from content where bestyn='?' ";
-			pstmt.setString(1,"N");
-			rs = pstmt.executeQuery();
-			while(rs.next()) {
-				cvo.setCseq(rs.getInt("cseq"));
-				cvo.setTitle(rs.getString("title"));
-				cvo.setLocationNum(rs.getInt("locationnum"));
-				cvo.setArtist(rs.getString("artist"));
-				cvo.setImage(rs.getString("image"));
-				cvo.setContent(rs.getString("content"));
-				cvo.setCategory(rs.getInt("category"));
-				cvo.setAge(rs.getString("age"));
-				//bestList.add(cvo);
-			}
-		} catch (SQLException e) { e.printStackTrace();
-		} finally { Dbman.close(con, pstmt, rs); }
-		return list;
-	}
+	      
+		  ArrayList<ContentVO> list = new ArrayList<ContentVO>();
+	      String sql = "select * from content where bestyn=?";
+	      con = Dbman.getConnection();
+	      try {
+	         pstmt = con.prepareStatement(sql);
+	         pstmt.setString(1,"N");
+	         rs = pstmt.executeQuery();
+	         while(rs.next()) {
+	        	ContentVO cvo = new ContentVO();
+	            cvo.setCseq(rs.getInt("cseq"));
+	            cvo.setTitle(rs.getString("title"));
+	            cvo.setLocationNum(rs.getInt("locationnum"));
+	            cvo.setArtist(rs.getString("artist"));
+	            cvo.setImage(rs.getString("image"));
+	            cvo.setContent(rs.getString("content"));
+	            cvo.setCategory(rs.getInt("category"));
+	            cvo.setAge(rs.getString("age"));
+	            cvo.setBestyn(rs.getString("bestyn").charAt(0));
+	            list.add(cvo);
+	         }
+	      } catch (SQLException e) { e.printStackTrace();
+	      } finally { Dbman.close(con, pstmt, rs); }
+	      return list;
+	   }
 	
 	public ArrayList<ContentVO> getConsert() {
 		ArrayList<ContentVO> concert = new ArrayList<ContentVO>();
@@ -52,7 +53,19 @@ public class ContentDao {
 			pstmt = con.prepareStatement(sql);
 			pstmt.setInt(1, 1);
 			rs = pstmt.executeQuery();
-			
+			while(rs.next()) {
+				ContentVO cvo = new ContentVO();
+				cvo.setCseq(rs.getInt("cseq"));
+				cvo.setTitle(rs.getString("title"));
+				cvo.setLocationNum(rs.getInt("locationnum"));
+				cvo.setArtist(rs.getString("artist"));
+				cvo.setImage(rs.getString("image"));
+				cvo.setContent(rs.getString("content"));
+				cvo.setCategory(rs.getInt("category"));
+				cvo.setAge(rs.getString("age"));
+				cvo.setBestyn(rs.getString("bestyn").charAt(0));
+				concert.add(cvo);
+			}
 		} catch (SQLException e) { e.printStackTrace();
 		} finally { Dbman.close(con, pstmt, rs); }
 		return concert;
@@ -111,6 +124,32 @@ public class ContentDao {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}finally {Dbman.close(con, pstmt, rs);}
+		return list;
+	}
+
+	public ArrayList<ContentVO> getMusical() {
+		ArrayList<ContentVO> list	=	new ArrayList<ContentVO>();
+		con = Dbman.getConnection();
+		String sql = "select * from content where category = 2";
+		ContentVO cvo = null;
+		try {
+			pstmt = con.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				cvo = new ContentVO();
+				cvo.setCseq(rs.getInt("cseq"));
+				cvo.setTitle(rs.getString("title"));
+				cvo.setLocationNum(rs.getInt("locationnum"));
+				cvo.setArtist(rs.getString("artist"));
+				cvo.setImage(rs.getString("image"));
+				cvo.setContent(rs.getString("content"));
+				cvo.setCategory(rs.getInt("category"));
+				cvo.setAge(rs.getString("age"));
+				cvo.setBestyn(rs.getString("bestyn").charAt(0));
+				list.add(cvo);
+			}
+		} catch (SQLException e) { e.printStackTrace();
+		} finally { Dbman.close(con, pstmt, rs); }
 		return list;
 	}
 	
