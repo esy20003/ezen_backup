4909192aeada2deafa9c1405525833810e773b00
KuMMii <4.prgmng@gmail.com>
2023-05-25 18:30:33 +0900

    Fix: 선택한 값 저장하고 넘기기 해결
    sessionStorage로 해결!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- ezen_pj/src/main/webapp/apply_register/apply_register.js
+++ ezen_pj/src/main/webapp/apply_register/apply_register.js
@@ -17,38 +17,71 @@
  
  function select_category(category){
 	 switch(category){
-		 case 0: location.href="ticket.do?command=categorySelect&category=0"; break;
-		 case 1: location.href="ticket.do?command=categorySelect&category=1"; break;
-		 case 2: location.href="ticket.do?command=categorySelect&category=2"; break;
-		 case 3: location.href="ticket.do?command=categorySelect&category=3"; break;
-		 case 4: location.href="ticket.do?command=categorySelect&category=4"; break;
-		 case 5: location.href="ticket.do?command=categorySelect&category=5"; break;
+		 case 0: location.href="ticket.do?command=categorySelect&category=0";
+		 sessionStorage.removeItem('selectedDate');
+		 sessionStorage.removeItem('selectedTime');
+		 sessionStorage.removeItem('selectedArea');
+		  break;
+		 case 1: location.href="ticket.do?command=categorySelect&category=1";
+		 sessionStorage.removeItem('selectedDate');
+		 sessionStorage.removeItem('selectedTime');
+		 sessionStorage.removeItem('selectedArea');
+		  break;
+		 case 2: location.href="ticket.do?command=categorySelect&category=2"; 
+		 sessionStorage.removeItem('selectedDate');
+		 sessionStorage.removeItem('selectedTime');
+		 sessionStorage.removeItem('selectedArea');
+		 break;
+		 case 3: location.href="ticket.do?command=categorySelect&category=3"; 
+		 sessionStorage.removeItem('selectedDate');
+		 sessionStorage.removeItem('selectedTime');
+		 sessionStorage.removeItem('selectedArea');
+		 break;
+		 case 4: location.href="ticket.do?command=categorySelect&category=4"; 
+		 sessionStorage.removeItem('selectedDate');
+		 sessionStorage.removeItem('selectedTime');
+		 sessionStorage.removeItem('selectedArea');
+		 break;
+		 case 5: location.href="ticket.do?command=categorySelect&category=5"; 
+		 sessionStorage.removeItem('selectedDate');
+		 sessionStorage.removeItem('selectedTime');
+		 sessionStorage.removeItem('selectedArea');
+		 break;
 	 }
  }
 
 
 function applyFormCheck(){
-	
+	var date=sessionStorage.getItem('selectedDate');
+	var time=sessionStorage.getItem('selectedTime');
+	var area=sessionStorage.getItem('selectedArea');
 	if(document.getElementById('cseq').value==""){
 		alert("공연을 선택해 주세요.");
 		return false;
-	}else if(document.date.value==""){
+	}else if(sessionStorage.getItem('selectedDate')==null){
 		alert("공연 날짜를 선택해 주세요.");
 		return false;
-	}else if(document.getElementById('time').value==""){
+	}else if(sessionStorage.getItem('selectedTime')==null){
 		alert("공연 시간을 선택해 주세요.");
 		return false;
-	}else if(document.getElementById('area').value==""){
+	}else if(sessionStorage.getItem('selectedArea')==null){
 		alert("공연 구역을 선택해 주세요.");
 		return false;
 	}else{
 		return true;	
 	}	
 }
- 
- function datecheck() {
-	alert(document.getElementById('cseq').value);
-	alert(document.getElementById('date1').value);
+
+function saveDate(num){
+	
+	sessionStorage.setItem('selectedDate',document.getElementById('date'+num).value);
 }
+function saveTime(num){
+	sessionStorage.setItem('selectedTime',document.getElementById('time'+num).value);
+}
+function saveArea(num){
+	sessionStorage.setItem('selectedArea',document.getElementById('area'+num).value);
+}
+ 
  
  
